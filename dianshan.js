fd59eba0c6351bbcf5e18d397815b3f7b97e0db5
syt5968 <18702198350@163.com>
2016-08-01 01:00:29 +0800

    电商网站
    简单的电商网站

---
+++ dianshan.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+    $(function(){$(".shop li").bind("mouseover",function(){$(".none").css("display","block")})
+});
+$(function(){$(".shop li").bind("mouseout",function(){$(".none").css("display","none")})
+});
+$(function(){$(".none").bind("mouseover",function(){$(".none").css("display","block")})
+});
+$(function(){$(".none").bind("mouseout",function(){$(".none").css("display","none")})
+});
